9c366917e5474c9f27707f6b224042931d37ba0b
Cosimo Bovi <cosimo.bovi@gmail.com>
2024-03-07 09:45:05 +0100

    test

--- Snake - Agent Nel Main/mainGame.py
+++ Snake - Agent Nel Main/mainGame.py
@@ -39,7 +39,9 @@ def main():
             quit() # chiude il programma
         gameUI.update_ui()  # Aggiorna l'interfaccia utente del gioco
 
+        
         print(agent.get_state())
+
 # Se il modulo è eseguito come script principale
 if __name__ == "__main__":
     main()  # Avvia la funzione principale
