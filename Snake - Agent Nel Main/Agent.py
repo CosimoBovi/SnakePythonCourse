ec689cee9bdf9ccf2d01ecd42893a48f3dfd6b56
Cosimo Bovi <cosimo.bovi@gmail.com>
2024-03-08 10:44:38 +0100

    spiegazione agent random

--- Snake - Agent Nel Main/Agent.py
+++ Snake - Agent Nel Main/Agent.py
@@ -56,22 +56,31 @@ class Agent:
         return np.array(state, dtype=bool)
 
    
+    # Metodo per ottenere l'azione da intraprendere
     def get_action(self):
-        final_move = [0,0,0]
-        move = random.randint(0, 2)
-        final_move[move] = 1
-        return final_move
-    
-     
-    def play_step(self,action):
-        moveIdx=action.index(max(action))
+        '''
+            Per decidere la mossa da compiere utilizziamo un vettore, dove ogni indice rappresenta un'azione:
+                0: diritto
+                1: gira a destra
+                2: gira a sinistra
+            Tutti e tre gli elementi possono avere un valore, e sarà scelto il massimo
+        '''
+        final_move = [0, 0, 0]  
+        move = random.randint(0, 2)  # Sceglie casualmente una mossa tra 0, 1 e 2
+        final_move[move] = 1  # Imposta la mossa scelta come attiva (1)
+        return final_move  # Restituisce la lista delle mosse finali
 
-        result=ActionResult.GO
-        if moveIdx==0:
-            result = self.game.play_step(Action.STRAIGHT)
-        elif moveIdx==1:
-            result = self.game.play_step(Action.RIGHT)
-        elif moveIdx==2:
-            result = self.game.play_step(Action.LEFT)
+    # Metodo per eseguire un passo nel gioco in base all'azione scelta
+    def play_step(self, action):
+        moveIdx = action.index(max(action))  # Trova l'indice della mossa con il valore massimo
 
-        return result
+        result = ActionResult.GO  # Inizializza il risultato dell'azione come 'GO'
+        # Esegue il passo del gioco in base alla mossa scelta
+        if moveIdx == 0:
+            result = self.game.play_step(Action.STRAIGHT)  # Muove dritto
+        elif moveIdx == 1:
+            result = self.game.play_step(Action.RIGHT)  # Gira a destra
+        elif moveIdx == 2:
+            result = self.game.play_step(Action.LEFT)  # Gira a sinistra
+
+        return result  # Restituisce il risultato dell'azione
