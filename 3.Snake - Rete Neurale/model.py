960e130aebc97d1331575480a37d2b3d6c321226
Cosimo Bovi <cosimo.bovi@gmail.com>
2024-03-11 15:42:46 +0100

    modifica struttura

--- 3.Snake - Rete Neurale/model.py
+++ 3.Snake - Rete Neurale/model.py
@@ -1,6 +1,7 @@
+import torch
 import torch.nn as nn 
 import torch.optim as optim
-import torch.nn.functional as F  #
+import torch.nn.functional as F  
 
 # Definizione di un modello di rete neurale lineare
 class Linear_QNet(nn.Module):
@@ -16,56 +17,3 @@ class Linear_QNet(nn.Module):
         x = F.relu(self.linear1(x))  # Applica la funzione di attivazione ReLU al primo layer lineare
         x = self.linear2(x)  # Passa l'output del primo layer al secondo layer lineare senza attivazione
         return x  # Restituisce l'output del modello
-
-
-class QTrainer:
-
-    def __init__(self, model, lr=0.001):
-
-        # Imposta il tasso di apprendimento
-        self.lr = lr
-        
-        # Inizializza il modello
-        self.model = model
-
-          
-        # Definisce la funzione di loss Mean Squared Error Loss
-        self.criterion = nn.MSELoss()
-
-        # Crea un ottimizzatore Adam per aggiornare i pesi del modello
-        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
-      
-
-    def train_step(self,state):
-        
-        '''
-            1) Ottieni le predizioni del modello per lo stato in input.
-            2) Applica formule per determinare l'azione più probabile da prendere.
-            3) Calcola l'errore tra le predizioni del modello e l'azione target.
-            4) Aggiorna i pesi e i bias del modello per minimizzare l'errore.
-
-
-        
-        '''
-
-        # Ottieni le predizioni del modello per lo stato corrente
-        pred = self.model(state)
-
-        # Effettua una copia delle predizioni per utilizzarle come target
-        target = pred.clone()
-
-        '''
-            Codice per calcolare l'azione più probabile e sostituirla nel target
-        '''
-
-        # Azzeramento dei gradienti accumulati in precedenza
-        self.optimizer.zero_grad()
-
-        # Calcola l'errore tra le predizioni del modello e il target
-        loss = self.criterion(target, pred)
-
-        # Calcola i gradienti dell'errore rispetto ai pesi e ai bias del modello
-        loss.backward()
-
-        # Aggiorna i pesi e i bias del modello utilizzando l'ottimizzatore
-        self.optimizer.step()
