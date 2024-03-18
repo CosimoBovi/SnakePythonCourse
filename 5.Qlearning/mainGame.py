44b11992817167c91523ad25504ac3d84d7e1a70
Cosimo Bovi <cosimo.bovi@gmail.com>
2024-03-18 09:05:41 +0100

    exnumber

--- 5.Qlearning/mainGame.py
+++ 5.Qlearning/mainGame.py
@@ -2,33 +2,6 @@ from SnakeGameUI import *
 from SnakeModel import *
 from Agent import *
 
-
-# Definizione di una funzione per determinare l'azione da intraprendere in base agli input dell'utente
-def findAction(game):
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                if game.direction == Direction.LEFT:
-                    return Action.RIGHT
-                if game.direction == Direction.RIGHT:
-                    return Action.LEFT
-            elif event.key == pygame.K_s:
-                if game.direction == Direction.LEFT:
-                    return Action.LEFT
-                if game.direction == Direction.RIGHT:
-                    return Action.RIGHT
-            elif event.key == pygame.K_a:
-                if game.direction == Direction.UP:
-                    return Action.LEFT
-                if game.direction == Direction.DOWN:
-                    return Action.RIGHT
-            elif event.key == pygame.K_d:
-                if game.direction == Direction.UP:
-                    return Action.RIGHT
-                if game.direction == Direction.DOWN:
-                    return Action.LEFT
-    return Action.STRAIGHT
-
 # Funzione principale del programma
 def main():
     game = SnakeGame()  # Crea un nuovo gioco 
